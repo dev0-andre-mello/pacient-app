e3662b3b5abf11a0b57761d697bfc88927aea8f6
dev0-andre-mello <punchmchn578@hotmail.com>
2025-07-28 16:06:40 -0300

    feat: save pacient data into a CSV.

---
+++ app/data_handler.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+CSV_FILE = 'pacientes.csv'
+
+def salvar_pacientes(nome: str, email: str):
+
+    novo_paciente = {'Name': nome, 'Email': email}
+
+    if os.path.exists(CSV_FILE):
+        df = pd.read_csv(CSV_FILE)
+        df = pd.concat([df, pd.DataFrame([novo_paciente])], ignore_index=True)
+
+    else:
+        df = pd.DataFrame([novo_paciente])
+
+    df.to_csv(CSV_FILE, index=False)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from data_handler import salvar_pacientes
 
 with st.sidebar:
     st.title("Menu")
@@ -8,7 +9,12 @@ with st.sidebar:
 
     if opção == "Cadastrar paciente":
         st.subheader("Cadastro de paciente")
-        st.write("Formulário aqui...")
+        nome = st.text_input('Nome do paciente')
+        email = st.text_input('Email do paciente')
+
+        if nome and email:
+            salvar_pacientes(nome, email)
+            st.success(f'Paciente {nome} cadastrado com o email {email}')
 
     elif opção == "Visualizar pacientes":
         st.subheader("Lista de pacientes")
